d2cd1e4e7fb0d1b5e81af444f002c123075777ad
Simon Thörnqvist <simon.thornqvist@gmail.com>
2021-08-31 17:01:22 +0200

    init public HCL interface

--- lib/hcl.ex
+++ lib/hcl.ex
@@ -4,15 +4,42 @@ defmodule HCL do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HCL.hello()
-      :world
-
   """
-  def hello do
-    :world
+  @spec from_file(Path.t()) :: {:ok, HCL.Ast.Body.t()} | {:error, term()}
+  def from_file(path) do
+    with {:ok, bin} <- File.read(path),
+         {:ok, _body} = return <- HCL.Parser.parse(bin) do
+      return
+    end
+  end
+
+  @doc """
+  """
+  @spec from_file!(Path.t()) :: HCL.Ast.Body.t()
+  def from_file!(path) do
+    case from_file(path) do
+      {:ok, body} -> body
+      {:error, reason} -> raise "err #{inspect(reason)}"
+    end
+  end
+
+  @doc """
+  """
+  @spec from_binary(binary()) :: {:ok, HCL.Ast.Body.t()} | {:error, term()}
+  def from_binary(bin) do
+    with {:ok, _body} = return <- HCL.Parser.parse(bin) do
+      return
+    end
+  end
+
+  @spec from_binary!(binary()) :: HCL.Ast.Body.t()
+  def from_binary!(bin) do
+    case from_binary(bin) do
+      {:ok, body} ->
+        body
+
+      {:error, reason, {line, _line_offset}, _} ->
+        raise "err #{inspect(reason)} line: #{inspect(line)}"
+    end
   end
 end
