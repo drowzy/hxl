18f2f5ebe1ccd052c4497513b90e25c5927fc2a4
Simon Thörnqvist <simon.thornqvist@gmail.com>
2021-10-01 09:50:59 +0200

    lexer tabs + spaces test

--- test/hxl/lexer_test.exs
+++ test/hxl/lexer_test.exs
@@ -7,4 +7,11 @@ defmodule HXL.LexerTest do
       assert length(out) > 0
     end
   end
+
+  test "can lex output containing tabs + spaces" do
+    for hcl <- ["\ta=1\s", "\t\sa=1\t\s"] do
+      assert {:ok, out, <<>>, _ctx, _, _} = HXL.Lexer.tokenize(hcl)
+      assert length(out) > 0
+    end
+  end
 end
